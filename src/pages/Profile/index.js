adab108647853a7e075e2a346c5d77451cba4169
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-22 16:39:43 -0300

    finished

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import Header from '~/components/Header';
 
 import { Container } from './styles';
 
 export default function Profile() {
   return (
     <Background>
+      <Header />
       <Container />
     </Background>
   );
