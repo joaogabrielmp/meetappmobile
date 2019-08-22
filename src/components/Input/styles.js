04bc81aabd8bd46ea583aabd8b7462b0b457406e
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-22 11:52:36 -0300

    finished

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 0 15px;
+  height: 46px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
 
-export const TextInput = styled.Text``;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextInput = styled.TextInput.attrs({
+  placeholderTextColor: 'rgba(255, 255, 255, 0.8)',
+})`
+  flex: 1;
+  font-size: 15px;
+  margin-left: 10px;
+  color: #fff;
+`;
