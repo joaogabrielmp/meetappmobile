2f3587e1fb8a4bdf3bb969a8cceaa0bed96eaec3
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-22 10:43:05 -0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+
+import Routes from './routes';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+  return <Routes />;
 }
