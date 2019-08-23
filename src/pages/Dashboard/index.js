ef16b1eefefc0b19baf810da186762d03bf36958
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-23 15:14:42 -0300

    up

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { withNavigationFocus } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 import DatePicker from '~/components/DatePicker';
 
-import { Container } from './styles';
+import { Container, Button } from './styles';
 
 function Dashboard() {
   const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
 
   return (
     <Background>
       <Header />
       <Container>
-        {/* <DatePicker date={date} onChange={setDate} /> */}
+        <Button onPress={handlePrevDay}>
+          <Icon name="chevron-left" size={45} color="#FFF" />
+        </Button>
+        <DatePicker date={date} onChange={setDate} />
+        {/* <strong>{dateFormatted}</strong> */}
+        <Button onPress={handleNextDay}>
+          <Icon name="chevron-right" size={45} color="#FFF" />
+        </Button>
       </Container>
     </Background>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,12 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-self: center;
+  align-items: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  /* margin-top: 20px; */
+`;
