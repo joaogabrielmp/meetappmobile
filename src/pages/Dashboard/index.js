4ba3c253ad82ef7f0c40ae68306a793f0b03ba10
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-23 15:01:15 -0300

    up

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withNavigationFocus } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import Header from '~/components/Header';
+import DatePicker from '~/components/DatePicker';
 
 import { Container } from './styles';
 
 function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <Background>
       <Header />
-      <Container />
+      <Container>
+        {/* <DatePicker date={date} onChange={setDate} /> */}
+      </Container>
     </Background>
   );
 }
