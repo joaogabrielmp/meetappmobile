46cebbe6e451b87c7bbaa408299daa8cca8aea61
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-26 14:33:06 -0300

    up

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -6,23 +6,43 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
 import Header from '~/components/Header';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
 import { Container, Form, FormInput, Separator, SubmitButton } from './styles';
 
 export default function Profile() {
   const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
 
   const emailRef = useRef();
   const passwordRef = useRef();
   const oldPasswordRef = useRef();
   const confirmPasswordRef = useRef();
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
   const [oldPassword, setOldPassword] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  function handleSubmit() {}
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
+  }
 
   return (
     <Background>
@@ -56,7 +76,7 @@ export default function Profile() {
           <Separator />
 
           <FormInput
-            icon="lock-outline"
+            icon="lock"
             secureTextEntry
             placeholder="Senha atual"
             ref={oldPasswordRef}
@@ -67,7 +87,7 @@ export default function Profile() {
           />
 
           <FormInput
-            icon="lock-outline"
+            icon="lock-open"
             secureTextEntry
             placeholder="Nova senha"
             ref={passwordRef}
